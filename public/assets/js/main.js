013f122c2b8bd935577b53cea9bc22aae100d69d
bil <bilal23@Host-003.lan>
2024-02-12 02:28:35 +0100

    overflow-x: hidden

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,75 +1,66 @@
-if (window.innerWidth > 900) {
-  document.addEventListener('DOMContentLoaded', function(e) {
-      e.preventDefault()
-      moveSection()
-  })
-  
-  document.addEventListener('wheel', upDownScroll);
-  document.addEventListener('keydown', keyUpDownLeftRight);
-  
-  let sections = document.querySelectorAll('section');
-  const nbreSection = sections.length - 1;
-  let currentSection = 0;
-  let isScrollingAllowed = true;
-  
-  function upDownScroll(event) {
-      if (!isScrollingAllowed) return;
-      isScrollingAllowed = false;
-      setTimeout(() => isScrollingAllowed = true, 400);
-      if (event.deltaY > 0 && currentSection < nbreSection) {
-          moveSection('down');
-      } else if (event.deltaY < 0 && currentSection > 0) {
-          moveSection('up');
-      }
-  }
-  
-  function keyUpDownLeftRight(event) {
-      if (event.key === "ArrowDown" || event.key === "ArrowRight") {
-          if (currentSection < nbreSection) moveSection('down');
-      } else if (event.key === "ArrowUp" || event.key === "ArrowLeft") {
-          if (currentSection > 0) moveSection('up');
-      }
-  }
-  
-  
-  function moveSection(direction) {
-      let previousSection = currentSection;
-      /*const color_header = ["#ccf381", "#3d155f","#1D3B31"]
-      let header = document.querySelectorAll('.top_bar a')*/
-     
-      
-      
-      if (direction === 'down') {
-          if (currentSection < nbreSection) {
-              currentSection++;
-          }
-      } else {
-          if (currentSection > 0) {
-              currentSection--;
-          }
-      }
-      let etat = previousSection === currentSection
-      //console.log(color_header[Math.min(currentSection,2)])
-  
-      
-      // Gérer l'animation de la section précédente
-      if (!etat) {
-          sections[previousSection].classList.remove('active');
-          if (direction === 'up') {
-              sections[previousSection].style.animation = 'slideOutToTop 0.9s forwards';
-          }
-      }
-      // Gérer l'animation de la section actuelle
-      sections[currentSection].style.display = 'flex';
-      if (direction === 'down') {
-          sections[currentSection].style.animation = 'slideInFromBottom 0.9s none';
-      }
-      sections[currentSection].classList.add('active');
-      /*header.forEach((element) => {
-          setTimeout(() =>{element.style.color= color_header[Math.min(currentSection,2)]},900)
-          
-      })*/
-  }
-  }
-  
-  
+function checkWindowSize() {
+    if (window.innerWidth > 900) {
+
+        document.addEventListener('wheel', upDownScroll);
+        document.addEventListener('keydown', keyUpDownLeftRight);
+
+        let sections = document.querySelectorAll('section');
+        const nbreSection = sections.length - 1;
+        let currentSection = 0;
+        let isScrollingAllowed = true;
+
+        function upDownScroll(event) {
+            if (!isScrollingAllowed) return;
+            isScrollingAllowed = false;
+            setTimeout(() => isScrollingAllowed = true, 400);
+            if (event.deltaY > 0 && currentSection < nbreSection) {
+                moveSection('down');
+            } else if (event.deltaY < 0 && currentSection > 0) {
+                moveSection('up');
+            }
+        }
+
+        function keyUpDownLeftRight(event) {
+            if (event.key === "ArrowDown" || event.key === "ArrowRight") {
+                if (currentSection < nbreSection) moveSection('down');
+            } else if (event.key === "ArrowUp" || event.key === "ArrowLeft") {
+                if (currentSection > 0) moveSection('up');
+            }
+        }
+
+
+        function moveSection(direction) {
+            let previousSection = currentSection;
+
+            if (direction === 'down') {
+                if (currentSection < nbreSection) {
+                    currentSection++;
+                }
+            } else {
+                if (currentSection > 0) {
+                    currentSection--;
+                }
+            }
+            let etat = previousSection === currentSection
+
+
+            // Gérer l'animation de la section précédente
+            if (!etat) {
+                sections[previousSection].classList.remove('active');
+                if (direction === 'up') {
+                    sections[previousSection].style.animation = 'slideOutToTop 0.9s forwards';
+                }
+            }
+            // Gérer l'animation de la section actuelle
+            sections[currentSection].style.display = 'flex';
+            if (direction === 'down') {
+                sections[currentSection].style.animation = 'slideInFromBottom 0.9s none';
+            }
+            sections[currentSection].classList.add('active');
+        }
+    }
+
+}
+document.addEventListener('DOMContentLoaded', checkWindowSize);
+
+window.addEventListener('resize', checkWindowSize);
